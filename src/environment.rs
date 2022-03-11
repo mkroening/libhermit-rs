66b81c185dbddf173f2291d40aed2f9b085cdc3d
Martin Kröning <m.kroening@hotmail.de>
2022-03-11 13:42:27 +0100

    WIP

--- src/environment.rs
+++ src/environment.rs
@@ -2,122 +2,122 @@
 //! vs. multi-kernel, hypervisor, etc.) as well as central parsing of the
 //! command-line parameters.
 
+use alloc::{boxed::Box, string::String, vec::Vec};
+use core::{slice, str};
+
+use once_cell::race::OnceBox;
+
 pub use crate::arch::kernel::{
 	get_base_address, get_cmdline, get_cmdsize, get_image_size, get_ram_address, get_tls_align,
 	get_tls_filesz, get_tls_memsz, get_tls_start, is_single_kernel, is_uhyve,
 };
 
-use alloc::string::String;
-use alloc::vec::Vec;
-use core::{slice, str};
+pub fn init() {
+	let cli = parse_command_line();
 
-static mut COMMAND_LINE_CPU_FREQUENCY: Option<u16> = None;
-static mut IS_PROXY: bool = false;
-static mut COMMAND_LINE_ENVIRONMENT: Vec<String> = Vec::new();
-static mut COMMAND_LINE_APPLICATION: Option<Vec<String>> = None;
-static mut COMMAND_LINE_PATH: Option<String> = None;
+	// Uhyve or baremetal implies unikernel mode and no communication with "proxy".
+	// Else we are running side-by-side to Linux, which implies communication with "proxy".
+	let is_proxy = !is_uhyve() && !is_single_kernel();
 
-unsafe fn parse_command_line() {
+	ENV.set(Box::new(Env { cli, is_proxy })).unwrap();
+}
+
+#[derive(Debug)]
+struct Env {
+	cli: Cli,
+	is_proxy: bool,
+}
+
+#[derive(Default, Debug)]
+struct Cli {
+	cpu_freq: Option<u16>,
+	env: Vec<String>,
+	path: Option<String>,
+	args: Vec<String>,
+}
+
+static ENV: OnceBox<Env> = OnceBox::new();
+
+// Returns the command line string.
+fn get_cmdline_str() -> Option<&'static str> {
 	let cmdsize = get_cmdsize();
-	if cmdsize == 0 {
-		return;
-	}
-
-	// Convert the command-line into a Rust string slice.
 	let cmdline = get_cmdline().as_ptr::<u8>();
-	let slice = unsafe { slice::from_raw_parts(cmdline, cmdsize) };
-	let cmdline_str = unsafe { str::from_utf8_unchecked(slice) };
+	if cmdline.is_null() {
+		None
+	} else {
+		// SAFETY: cmdline and cmdsize are valid forever.
+		let slice = unsafe { slice::from_raw_parts(cmdline, cmdsize) };
+		Some(str::from_utf8(slice).unwrap())
+	}
+}
 
-	// Split at spaces, but not while in quotes
-	let tokens = shell_words::split(cmdline_str).unwrap();
-	debug!("Got cmdline tokens as {:?}", tokens);
+fn parse_command_line() -> Cli {
+	panic!("FOO");
 
-	let mut tokeniter = tokens.into_iter();
-	while let Some(token) = tokeniter.next() {
-		match token.as_str() {
+	let mut cli = Cli::default();
+
+	// TODO: Replace with let-else once stable.
+	let cmdline_str = get_cmdline_str();
+	if cmdline_str.is_none() {
+		return cli;
+	}
+	let cmdline_str = cmdline_str.unwrap();
+
+	let words = shell_words::split(cmdline_str).unwrap();
+	debug!("cli_words = {words:?}");
+
+	let mut words = words.into_iter();
+	let expect_arg = |arg: Option<String>, name: &str| {
+		arg.unwrap_or_else(|| {
+			panic!("The argument '{name}' requires a value but none was supplied")
+		})
+	};
+	while let Some(word) = words.next() {
+		match word.as_str() {
 			"-freq" => {
-				let mhz_str = tokeniter.next().expect("Invalid -freq command line");
-				unsafe {
-					COMMAND_LINE_CPU_FREQUENCY = mhz_str.parse().ok();
-				}
+				let freq = expect_arg(words.next(), word.as_str());
+				cli.cpu_freq = Some(freq.parse().unwrap());
 			}
-			"-ip" => unsafe {
-				COMMAND_LINE_ENVIRONMENT.push(format!(
-					"HERMIT_IP={}",
-					tokeniter.next().expect("Invalid -ip command line")
-				));
-			},
-			"-mask" => unsafe {
-				COMMAND_LINE_ENVIRONMENT.push(format!(
-					"HERMIT_MASK={}",
-					tokeniter.next().expect("Invalid -mask command line")
-				));
-			},
-			"-gateway" => unsafe {
-				COMMAND_LINE_ENVIRONMENT.push(format!(
-					"HERMIT_GATEWAY={}",
-					tokeniter.next().expect("Invalid -gateway command line")
-				));
-			},
-			"-proxy" => unsafe {
-				IS_PROXY = true;
-			},
-			"--" => {
-				// Collect remaining arguments as applications argv
-				//ToDo -> we know the length here, so we could (should convert this into a safe
-				// rust type (at least for rust applications)
-				unsafe {
-					COMMAND_LINE_APPLICATION = Some(tokeniter.collect());
-				}
-				break;
+			"-ip" => {
+				let ip = expect_arg(words.next(), word.as_str());
+				cli.env.push(format!("HERMIT_IP={ip}"));
 			}
-			_ if unsafe { COMMAND_LINE_PATH.is_none() } => {
-				// Qemu passes in the kernel path (rusty-loader) as first argument
-				unsafe { COMMAND_LINE_PATH = Some(token) }
+			"-mask" => {
+				let mask = expect_arg(words.next(), word.as_str());
+				cli.env.push(format!("HERMIT_MASK={mask}"));
 			}
-			_ => {
-				warn!("Unknown cmdline option: {} [{}]", token, cmdline_str);
+			"-gateway" => {
+				let gateway = expect_arg(words.next(), word.as_str());
+				cli.env.push(format!("HERMIT_GATEWAY={gateway}"));
 			}
+			"--" => cli.args.extend(&mut words),
+			_ if cli.path.is_none() => cli.path = Some(word),
+			word => panic!(
+				"Found argument '{word}' which wasn't expected, or isn't valid in this context
+			
+ 		If you tried to supply `{word}` as a value rather than a flag, use `-- {word}`"
+			),
 		};
 	}
+
+	cli
 }
 
-/// Returns the cmdline argument passed in after "--"
-pub fn get_command_line_argv() -> Option<&'static [String]> {
-	unsafe { COMMAND_LINE_APPLICATION.as_deref() }
-}
-
-#[allow(dead_code)]
-/// Returns the first cmdline argument, if not otherwise recognized. With qemu this is the host-path to the kernel (rusty-loader)
-pub fn get_command_line_path() -> Option<&'static str> {
-	unsafe { COMMAND_LINE_PATH.as_deref() }
+/// CPU Frequency in MHz if given through the -freq command-line parameter.
+pub fn get_command_line_cpu_frequency() -> Option<u16> {
+	ENV.get().unwrap().cli.cpu_freq
 }
 
 pub fn get_command_line_envv() -> &'static [String] {
-	unsafe { COMMAND_LINE_ENVIRONMENT.as_slice() }
+	ENV.get().unwrap().cli.env.as_slice()
 }
 
-pub fn init() {
-	unsafe {
-		parse_command_line();
-
-		if is_uhyve() || is_single_kernel() {
-			// We are running under uhyve or baremetal, which implies unikernel mode and no communication with "proxy".
-			IS_PROXY = false;
-		} else {
-			// We are running side-by-side to Linux, which implies communication with "proxy".
-			IS_PROXY = true;
-		}
-	}
-}
-
-/// CPU Frequency in MHz if given through the -freq command-line parameter, otherwise zero.
-pub fn get_command_line_cpu_frequency() -> Option<u16> {
-	unsafe { COMMAND_LINE_CPU_FREQUENCY }
+/// Returns the cmdline argument passed in after "--"
+pub fn get_command_line_argv() -> &'static [String] {
+	ENV.get().unwrap().cli.args.as_slice()
 }
 
 /// Whether HermitCore shall communicate with the "proxy" application over a network interface.
-/// Only valid after calling init()!
 pub fn is_proxy() -> bool {
-	unsafe { IS_PROXY }
+	ENV.get().unwrap().is_proxy
 }
